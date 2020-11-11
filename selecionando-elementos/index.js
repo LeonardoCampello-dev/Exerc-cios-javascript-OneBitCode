e539f0864879d96264ffd9573bb2c07af7040135
Leonardo Campello <leonardocampello.dev@gmail.com>
2020-11-11 17:16:07 -0300

    Selecting elements

---
+++ selecionando-elementos/index.js
@@ -0,0 +1,16 @@
+function show() {
+    const elementById = document.getElementById('name')
+    console.log(elementById.value)
+
+    const elementsByName = document.getElementsByName('phone')
+    console.log(elementsByName[0].value)
+
+    const elementsByQuerySelector = document.querySelectorAll('div#phones input[name="phone"]')
+    console.log(`
+        Phone 01: ${elementsByQuerySelector[0].value} 
+        Phone 02: ${elementsByQuerySelector[1].value}
+    `)
+
+    const elementsByTagName = document.getElementsByTagName('input')
+    console.log(elementsByTagName)
+}
